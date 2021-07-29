4a278cac97074b710c548abaae343461b0e545d7
tigist_alemu <tigist_alemu@tigistalemusAir.home>
2021-07-28 20:21:30 -0400

    product

--- data.js
+++ data.js
@@ -1,40 +1,18 @@
-function binarySearch(arr, item){
-    var first = 0, mid;
-    var last = arr.length - 1;
-    var found = false
-    var arrayIndex = -1
-    while( (first <= last) && !found){
-        mid = Math.floor((first + last) / 2)
-        if( arr[mid] == item){
-            arrayIndex = mid
-            found = true
-        }
-        else{
-            if(item < arr[mid])
-                last = mid - 1
-            else
-                first = mid + 1
-        }
-    }
-    if (found)
-         return item
-     else
-         return false
- }
- 
- function findSum(arr,value){
-    arr.sort(function(a, b) {
-         return a - b
-     })
- var index;
-  for(var j = 0; j<arr.length; j++){
-    index = binarySearch(arr,(value-arr[j]))
-    if(index!=false )
-      return [arr[j],value-arr[j]]
-  }
-   return false;
- }
- 
- 
- console.log(findSum([1,2,3,4],5))
- console.log(findSum([4,3,2,1],9))
+function findProduct(array){
+let left = 1
+let currentProduct
+let result = []
+for( let i = 0; i < array.length; i++){
+    currentProduct = 1
+   for(let j = i+1 ; j < array.length; j++){
+       currentProduct = array[j] * currentProduct
+   }
+
+   result.push(currentProduct*left)
+   left = left * array[i]
+}
+
+return result
+}
+
+console.log(findProduct([1,2,3]))
